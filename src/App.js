018615af2b74474d1a01bc4dce7fbb302addb96e
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2025-12-15 13:52:54 +0530

    Passing Props to Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import { ProductCard } from './components/ProductCard';
+import { ProductList } from './components/ProductList';
 
-
+ const product = {
+    imageSrc: 'images/iphone.png',
+    title: 'iPhone 15 Pro',
+    price: 999,
+    specifications: [
+      '6.1-inch display',
+      'A17 Pro chip',
+      'ProMotion technology',
+      'Up to 1TB storage'
+    ]
+  }
 
 function App() {
   return (
     <div className="App">
-      <ProductCard />
+      <ProductList>
+        <ProductCard product={product} />
+        <ProductCard product={product} />
+        <ProductCard product={product} />
+        <ProductCard product={product} />
+      </ProductList>
+      
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,40 +1,31 @@
-export function ProductCard(){
-
-  const product = {
-    imageSrc: 'images/iphone.png',
-    title: 'iPhone 15 Pro',
-    price: 999,
-    specifications: [
-      '6.1-inch display',
-      'A17 Pro chip',
-      'ProMotion technology',
-      'Up to 1TB storage'
-    ]
-  }
-
-  function getProductTitle(title){
-    return product.title;
-  }
-
-  return(
-    <article style={{border: '1px solid white', borderRadius: "8px", padding:"16px",textAlign:"center"}}>
-      <h2>{getProductTitle(product.title)}</h2>
+export function ProductCard(props) {
+  return (
+    <article
+      style={{
+        width: "100%",
+        border: "1px solid white",
+        borderRadius: "8px",
+        padding: "16px",
+        textAlign: "center",
+      }}
+    >
+      <h2>{props.product.title}</h2>
       <img
-        src={product.imageSrc}
-        alt={product.title}
+        src={props.product.imageSrc}
+        alt={props.product.title}
         width="128px"
         height="128px"
       />
 
       <p>Specification:</p>
-      <ul style={{listStyle:"none", padding:"0"}}>
-        <li>{product.specifications[0]}</li>
-        <li>{product.specifications[1]}</li>
-        <li>{product.specifications[2]}</li>
-        <li>{product.specifications[3]}</li>
+      <ul style={{ listStyle: "none", padding: "0" }}>
+        <li>{props.product.specifications[0]}</li>
+        <li>{props.product.specifications[1]}</li>
+        <li>{props.product.specifications[2]}</li>
+        <li>{props.product.specifications[3]}</li>
       </ul>
 
-      <button>Buy (From ${product.price})</button>
+      <button>Buy (From ${props.product.price})</button>
     </article>
   );
 }
