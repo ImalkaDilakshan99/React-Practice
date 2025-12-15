69e52d6ccf6fc8b2bf48e7aa65d7e9e56d159ee3
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2025-12-15 13:40:01 +0530

    using variables in JSX file

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,40 @@
 export function ProductCard(){
+
+  const product = {
+    imageSrc: 'images/iphone.png',
+    title: 'iPhone 15 Pro',
+    price: 999,
+    specifications: [
+      '6.1-inch display',
+      'A17 Pro chip',
+      'ProMotion technology',
+      'Up to 1TB storage'
+    ]
+  }
+
+  function getProductTitle(title){
+    return product.title;
+  }
+
   return(
-    <article>
-      <h2>iPhone 15 Pro</h2>
+    <article style={{border: '1px solid white', borderRadius: "8px", padding:"16px",textAlign:"center"}}>
+      <h2>{getProductTitle(product.title)}</h2>
       <img
-        src='images/iphone.png'
-        alt='iPhone 15 Pro'
+        src={product.imageSrc}
+        alt={product.title}
         width="128px"
         height="128px"
       />
 
       <p>Specification:</p>
-      <ul>
-        <li>6.1-inch display</li>
-        <li>A17 Pro chip</li>
-        <li>ProMotion technology</li>
-        <li>Up to 1TB storage</li>
+      <ul style={{listStyle:"none", padding:"0"}}>
+        <li>{product.specifications[0]}</li>
+        <li>{product.specifications[1]}</li>
+        <li>{product.specifications[2]}</li>
+        <li>{product.specifications[3]}</li>
       </ul>
 
-      <button>Buy (From $999)</button>
+      <button>Buy (From ${product.price})</button>
     </article>
   );
 }
